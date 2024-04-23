aa83200892c8b3eee61097ec78a7002a8e0678ef
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-04-23 15:11:13 +0000

    JavaScript self practice

---
+++ day1.js
@@ -0,0 +1,15 @@
+let myNumber = 54;
+let myString = 'Andrews';
+let myBoolean = true; // Assigns a true value to my boolean
+
+console.log(myNumber);
+console.log(myString);
+console.log(myBoolean);
+
+//Arithmetic operations
+let x = sum (4, 3);
+
+function sum (a, b){
+    return a + b
+}
+console.log(x);
