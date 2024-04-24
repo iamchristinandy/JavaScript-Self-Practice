15ff9b8ea354405cc12aca32ed8abf49b7234279
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-04-24 17:11:08 +0000

    day 3

--- day3.js
+++ day3.js
@@ -1,1 +1,5 @@
 // Conditional Statements
+/* #2 A function that takes a number as an argument and returns "Even" if the number is an even number
+and odd if the number is odd */
+
+// Yet to be done
