6b3182f92c62b54c2d8f385fc92d349903bf4d99
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-04-25 10:43:04 +0000

    day 3

--- day3.js
+++ day3.js
@@ -2,4 +2,16 @@
 /* #2 A function that takes a number as an argument and returns "Even" if the number is an even number
 and odd if the number is odd */
 
-// Yet to be done
+function checkEvenorOdd(num){
+    if (num % 2 === 0) {
+        // Explained as 'if the number divided by 2 doesn't give a remainder'
+        // Or if the number modulo 2 returns a 0
+        return "The number is an even number"
+    } else {
+        return "The number is odd"
+    }
+}
+console.log(checkEvenorOdd(14));
+// returns "The number is even"
+console.log(checkEvenorOdd(15));
+// returns "The number is odd"
