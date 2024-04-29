e17576909af79fa2c55584d0dcc28426d7e4e24e
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-04-29 15:30:03 +0000

    Now to objects

---
+++ day5.js
@@ -0,0 +1,31 @@
+// Moving to objects
+
+// Creating an object representing a person with properties
+
+let person = {
+    firstName: "Andrews",
+    surname: "Baah",
+    otherNames: "Kwafo",
+    age: 23,
+    profession: "frontend Developer with UI background",
+}
+console.log(person);
+
+//Combining the names properties in the object to form a sentence
+console.log(`${person.firstName} ${person.otherNames} ${person.surname} is my full name`);
+
+// Function that takes an object as an argument and prints out all its properties and values
+function printAllProperties(employee){
+    for (let property in employee){
+        if(employee.hasOwnProperty(property)){
+            console.log(`${property}: ${employee[property]}`);
+        }
+    }
+}
+
+let employee = {
+    name: "Aning Obed",
+    age: 23,
+    city: "Accra Ghana",
+};
+printAllProperties(employee);
