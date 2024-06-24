d975b8b73fa3e858aa7d7f2efcd0e3fe6052fa17
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-06-24 15:24:59 +0000

    more javascript pratices

---
+++ day10.js
@@ -0,0 +1,11 @@
+// Array Manipulation
+// A function that reverses an array
+
+function arrayReversal(arr){
+    return arr.reverse();
+}
+
+// Use in example
+let arr = [14, 15, 16, 17, 18];
+let reversedArray = arrayReversal(arr);
+console.log(reversedArray);
