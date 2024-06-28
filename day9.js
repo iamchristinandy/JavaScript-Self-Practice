3cd0000b385b50f57011fdc696f386fbe7e2fe23
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-06-28 15:30:43 +0000

    bubble sorted array test

--- day9.js
+++ day9.js
@@ -16,3 +16,28 @@ var students = [
 ];
 console.log(ReturnScores(students, 75)); // Prints out students who had a score of above 75
 console.log(ReturnScores(students, 65)); // Prints out students who had a score of above 65
+
+// Implementing a function that sorts an array of integers using a chosen sorting algorithm
+// preferably 'bubble sorting' algorithm
+
+function ArraySort(array){
+    let a = array.length;
+    // Traverse through all array elements
+    for (let i = 0; i < a; i++)
+    // Let i elements are already in place
+    for (let j = 0; j < a - i; j++){
+    // Traverse the array from 0 to a-i-1
+    // Swap if the element found is greater than the next element
+    if (array[j] > array[j+1]){
+        let temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+    }
+ }
+ return array;
+}
+
+// In an example
+let array = [64, 21, 19, 48, 10];
+let bubblesortedArr = ArraySort(array);
+console.log("Sorted array is ", bubblesortedArr);
