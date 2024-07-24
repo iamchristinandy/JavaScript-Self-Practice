792faf4adf14d7e1d53533b5d9ab514b37839d41
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-07-24 12:59:52 +0000

    Writing a funtion that dynamically adds elements to a list based on userinput

---
+++ script.js
@@ -0,0 +1,21 @@
+// Writing a funtion that dynamically adds elements to a list based on userinput
+function addItem(){
+    // Get input element and its value
+    const input = document.getElementById('userInput');
+    const inputValue = input.ariaValueMax;
+}
+
+// Checking of the property isn't empty
+if (inputValue.trim() !==''){
+    // Create a new list
+    const newItem = document.createElement('li');
+    newItem.textContent = inputValue;
+
+    // Clear the input field
+    input.value = '';
+} else {
+    alert('Please enter something')
+}
+
+// Attach an event listener to the button
+document.getElementById('addButton').addEventListener('click', addItem);
