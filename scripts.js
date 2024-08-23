b5cfdbf3dec08083488fe9be22af343a81196f63
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-08-23 17:12:10 +0000

    Bug Fixing of JS Code showing red sign about potential error

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Get the modal element
-var modal = document.getElementById("myModal");
+var modal = document.getElementById ("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("openModalButton");
