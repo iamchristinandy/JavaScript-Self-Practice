0fbb62b8478ab5f0790cd2f6aab7b5f4ec3d85a3
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-08-27 16:58:08 +0000

    Comments on just ended exercise session

--- scripts2.js
+++ scripts2.js
@@ -1,5 +1,6 @@
 // Function to handle keydown events
 
+// This script demonstrates how to get keyboard events in Javascript
 function handleKeydown (event){
     // To get the key pressed
     const key = event.key
@@ -31,3 +32,9 @@ function handleKeydown (event){
 
 // Attach the event listener to the window
 window.addEventListener('keydown', handleKeydown);
+
+// `keydown`: Fires when a key is pressed down.
+// `keyup`: Fires when a key is released.
+// `keypress`: Deprecated, but was used to detect keys that produce a character
+// `event.key`: Returns the key value as a string. Example: "Enter"
+// `event.code`: Returns the physical keycode 9. Example: "KeyB", "ArrowLeft"
