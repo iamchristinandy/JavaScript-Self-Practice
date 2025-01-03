2ac183c7ed37d3696821c76e15e4bbe14c81b227
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2025-01-03 12:49:12 +0000

    Trying my hands on Javascript again

--- day1.js
+++ day1.js
@@ -13,3 +13,12 @@ function sum (a, b){
     return a + b
 }
 console.log(x);
+
+// Pretty long time I tried this code. Indeed, if you stop practising code, you can really forget your own codes you wrote
+// let's begin again for me to be abreast with it.
+
+let myAge = 24;
+let myFullName = 'Andrews Baah Kwafo';
+let myGender = 'male';
+
+console.log(`I am ${myFullName}. I am ${myAge} years of age and I am a ${myGender}.`);
